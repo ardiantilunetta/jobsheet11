c5a8f031ccb7edc59a1b8890adf15c4020b8d70b
Nabila Ardianti Lunetta <nabilaardianti17@gmail.com>
2025-11-21 00:24:06 +0700

    P2. Modifikasi pilih menu

--- BioskopWithScanner18.java
+++ BioskopWithScanner18.java
@@ -4,27 +4,37 @@ public class BioskopWithScanner18 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int baris, kolom;
+        int baris, kolom, pilihan;
         String nama, next;
         String[][] penonton = new String[4][2];
 
-        while (true) {
-            System.out.print("Masukkan nama: ");
-            nama = sc.nextLine();
-            System.out.print("Masukkan baris: ");
-            baris = sc.nextInt();
-            System.out.print("Masukkan kolom: ");
-            kolom = sc.nextInt();
+        do {
+            System.out.println("---- MENU BIOSKOP ----");
+            System.out.println("1. Input data penonton");
+            System.out.println("2. Tampilkan daftar penonton");
+            System.out.println("3. Exit");
+            System.out.print("Pilih menu: ");
+            pilihan = sc.nextInt();
             sc.nextLine();
+        } while (pilihan != 3);
 
-            penonton[baris-1][kolom-1] = nama;
+        // while (true) {
+        //     System.out.print("Masukkan nama: ");
+        //     nama = sc.nextLine();
+        //     System.out.print("Masukkan baris: ");
+        //     baris = sc.nextInt();
+        //     System.out.print("Masukkan kolom: ");
+        //     kolom = sc.nextInt();
+        //     sc.nextLine();
 
-            System.out.print("Input penonton lainnya? (y/n): ");
-            next = sc.nextLine();
+        //     penonton[baris-1][kolom-1] = nama;
 
-            if (next.equalsIgnoreCase("n")) {
-                break;
-            }
-        }
+        //     System.out.print("Input penonton lainnya? (y/n): ");
+        //     next = sc.nextLine();
+
+        //     if (next.equalsIgnoreCase("n")) {
+        //         break;
+        //     }
+        // }
     }
 }
