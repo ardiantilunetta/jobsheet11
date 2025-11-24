db3f917959a95f151123af27601dbda981cb004a
Nabila Ardianti Lunetta <nabilaardianti17@gmail.com>
2025-11-24 23:05:40 +0700

    rata rata setiap responden

--- HasilSurvei18.java
+++ HasilSurvei18.java
@@ -4,25 +4,35 @@ public class HasilSurvei18 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int hasilsurvey[][] = new int[10][6];
+        int hasilsurvey[][] = new int[2][6];
 
-        for (int r = 0 ; r < 10 ; r++) {
+        for (int r = 0 ; r < 2 ; r++) {
             System.out.println("Responden "+(r+1));
 
-            for (int n = 0 ; n < 6 ; n++) {
-                
+            for (int p = 0 ; p < 6 ; p++) {
+
                 while (true) {
-                    System.out.print("Nilai pertanyaan " + (n+1) + " : ");
+                    System.out.print("Nilai pertanyaan " + (p+1) + " : ");
                     int nilai = sc.nextInt();
 
                     if (nilai >= 1 && nilai <= 5) {
-                        hasilsurvey[r][n] = nilai;
+                        hasilsurvey[r][p] = nilai;
                         break;
                     } else {
                         System.out.println("Tidak valid! Masukkan nilai 1-5");
                     }
                 }
             }
+        }
+
+        System.out.println("---- RATA RATA SETIAP RESPONDEN ----");
+        for (int r = 0 ; r < hasilsurvey.length ; r++) {
+            double responden = 0;
+
+            for (int p = 0 ; p < hasilsurvey[r].length ; p++) {
+                responden += hasilsurvey [r][p];
+            }
+            System.out.println("Responden "+(r+1)+" : "+(responden/hasilsurvey[r].length));
         }
     }
 }
